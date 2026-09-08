6608c64623ad83dbe198ff730dae3f17d068fef7
multi-user-Expense-Tracker-API <gooldyflora@gmail.com>
2026-09-08 14:04:50 +0700

    Update : update gemini models

--- app/services/model_factory.py
+++ app/services/model_factory.py
@@ -12,7 +12,7 @@ def get_configurable_llm_provider() -> BaseChatModel:
         if not settings.GEMINI_API_KEY:
             raise ValueError("Configuration Error: GEMINI_API_KEY variable is missing.")
         return ChatGoogleGenerativeAI(
-            model="gemini-2.5-flash",
+            model="gemini-3.6-flash",
             google_api_key=settings.GEMINI_API_KEY,
             temperature=0.1
         )
